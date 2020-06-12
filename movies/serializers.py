e2f9e0ab38d22894b86544a2ffe6782b1b60e6f2
김익한 <kt0831@naver.com>
2020-06-12 17:40:08 +0900

    Serializer Index

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,19 @@
 from rest_framework import serializers
 from accounts.serializers import UserSerializer
-from .models import Movie
+from .models import Movie, MovieRankComment
+
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('title', 'vote_average', 'poster_path', 'genres')
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class MovieRankCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieRankComment
+        fields = ('rank', 'content', 'user', 'movie')
+        
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import Movie, MovieRankComment
+from .serializers import MovieListSerializer, MovieDetailSerializer, MovieRankCommentSerializer
 # Create your views here.
 
+@api_view(['GET'])
 def index(request):
-    pass
+    movies = Movie.objects.all()
+    serializer = MovieListSerializer(movies, many=True)
+    return Response(serializer.data)
