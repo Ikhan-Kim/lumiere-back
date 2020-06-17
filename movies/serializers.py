913a275d357ee491207d272a0f1566a49b87af30
김익한 <kt0831@naver.com>
2020-06-17 22:44:05 +0900

    Final_commit

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,18 +2,19 @@ from rest_framework import serializers
 from accounts.serializers import UserSerializer
 from .models import Movie, MovieRankComment
 
-class MovieListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ('id', 'title', 'vote_average', 'poster_path', 'genres', 'backdrop_path')
-
 class MovieRankCommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
     class Meta:
         model = MovieRankComment
         # fields = '__all__'
         exclude = ['movie']
-        
+
+class MovieListSerializer(serializers.ModelSerializer):
+    comments = MovieRankCommentSerializer(many=True, read_only=True)
+    class Meta:
+        model = Movie
+        fields = '__all__'
+                
 class MovieDetailSerializer(serializers.ModelSerializer):
     comments = MovieRankCommentSerializer(many=True, read_only=True)
     class Meta:
